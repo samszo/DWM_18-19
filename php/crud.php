0b3d2c338c1ab214d1bcf98aa16f3d2f13481582
Samuel Szoniecky <samszon@gmail.com>
2019-02-22 13:57:02 +0100

    Update crud.php

--- php/crud.php
+++ php/crud.php
@@ -12,7 +12,7 @@ if( isset($_POST['code']) )
 
 // Formulation de la requête SQL :  insertion de données
     $sql= "INSERT INTO `geo` (`lat`, `lng`, `alt`, `acc`, `maj`, `obj`) 
-    VALUES ('$lat', '$lng', '$alt', '$acc', NOW(), '$obj');
+    VALUES ('$lat', '$lng', '$alt', '$acc', NOW(), '$obj')";
     
     // Exécution de la requête SQL : insertion de données
     if (mysqli_query($conn, $sql))
