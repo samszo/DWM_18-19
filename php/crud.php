ce7e6808def156108932c2ee8c43dc82b25c1a12
noeklk <43657082+noeklk@users.noreply.github.com>
2019-02-22 13:40:20 +0100

    2

--- php/crud.php
+++ php/crud.php
@@ -1,1 +1,6 @@
+<?php
 
+$insert_comm = "INSERT INTO commentaire (pseudo, email, texte, `date`, id_article) VALUES ('$pseud', '$mail', '$_comment', '$today','$ID_ART')";
+
+$affichart = "SELECT id, auteur, titre, texte, `date` FROM article WHERE id = '$ID_ART'";
+?>
