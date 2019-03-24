5c052e0f721009ae886a7c6151096dddc3b41548
RochDLY <roch.delannay@gmail.com>
2019-03-24 13:25:03 +0100

    Update tag.php

--- sonar/application/scripts/tag.php
+++ sonar/application/scripts/tag.php
@@ -2,9 +2,9 @@
 
 // Données de connexion serveur MySQL
 $servername = "localhost";
-$username = "***";
-$password = "***";
-$dbname = "spip_dwm_sonar";
+$username = "*";
+$password = "*";
+$dbname = "sonar";
 
 // Connexion au serveur MySQL
 $conn = mysqli_connect($servername, $username, $password, $dbname);
